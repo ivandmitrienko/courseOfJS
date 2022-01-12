270b7272166e00775669ffc80e63b896eec02f89
ivandmitrienko <oneblackcrystal@gmail.com>
2022-01-12 18:03:24 +0300

    initial commit

--- MOOD/MOOD.js
+++ MOOD/MOOD.js
@@ -26,3 +26,32 @@ function mood(colorsCount) {
 }
 
 mood(3);
+
+
+// "used strict"
+
+// function randomDiap(n,m) {
+//     return Math.floor(Math.random()*(m-n+1))+n;
+// }
+
+// function mood(colorsCount) {
+//     var colors=[ '', 'красный', 'оранжевый', 'жёлтый', 'зелёный', 'голубой', 'синий', 'фиолетовый' ];
+    
+
+
+//     console.log( 'цветов: ' + colorsCount );
+
+//     var usedColors = {};
+
+//     for ( var i=1; i<=colorsCount; i++ ) {
+//         do {
+//            var n = randomDiap(1,7);
+//         } while (n in usedColors)
+
+//        if(!(n in usedColors)) usedColors[n] = true;
+       
+//          console.log(n);
+//     }
+// }
+
+// mood(3);
