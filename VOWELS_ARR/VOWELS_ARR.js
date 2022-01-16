9cf8ea356fe55289d9748eee52efb1b19732c9f3
ivandmitrienko <oneblackcrystal@gmail.com>
2022-01-16 19:39:44 +0300

    initial commit

--- VOWELS_ARR/VOWELS_ARR.js
+++ VOWELS_ARR/VOWELS_ARR.js
@@ -8,14 +8,33 @@ function vowelsTenForEach(str) {
 
     let count = 0;
 
-    str.forEach((v) => {let value = v;
-    if(vowels.includes(value)){count++}});
+    str.forEach((v) => {if(vowels.includes(v)){count++}});
 
    
   
     return count; 
 }
 
+
+
+
+function vowelsTenFilter(str) {
+
+    let vowels = ["а", "о", "и", "е", "ё", "э", "ы", "у", "ю", "я"];
+
+    str = str.toLowerCase().split("");
+
+    let count = 0;
+
+    let arr = str.filter((v) => vowels.includes(v));
+
+   
+  
+    return arr.length;
+}
+
+
+
+
 console.log(vowelsTenForEach(prompt("Пожалуйста, введите строку","")));
-
-
+console.log(vowelsTenFilter(prompt("Пожалуйста, введите строку","")));
