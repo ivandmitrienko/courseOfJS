300ded142bdab97de08d5b8c5dafd7347f90a4ca
ivandmitrienko <oneblackcrystal@gmail.com>
2022-01-18 23:51:54 +0300

    initial commit

---
+++ DRINKS_HASH_FUNC/DRINKS_HASH_FUNC.js
@@ -0,0 +1,27 @@
+function HashStorageFunc() {
+    let self = this;
+    let privateHash = {};
+
+    self.addValue = function(key, value) {
+        privateHash[key] = value;
+    }
+
+    self.getValue = function(key) {
+        if(key in privateHash) {return privateHash[key];
+        } else {
+            return undefined;
+        }
+    }    
+
+        self.deleteValue = function(key) {
+            if(!(key in privateHash)) {delete privateHash[key];
+            return true;
+            } else { 
+            return false;
+            }
+        }    
+
+        self.getKeys() = function() {
+            return Object.keys(privateHash);
+        }
+}  
