64f13d0b77391dbe740461a97cb8c557c1b68bf5
ivandmitrienko <oneblackcrystal@gmail.com>
2022-01-20 22:23:57 +0300

    initial commit

--- DRINKS_HASH_FUNC/DRINKS_HASH_FUNC.js
+++ DRINKS_HASH_FUNC/DRINKS_HASH_FUNC.js
@@ -1,4 +1,4 @@
-// (function() {
+(function() {
 
     "use strict";
 
@@ -9,14 +9,14 @@
  
      self.addValue = function(key, value) {
          privateHash[key] = value;
-     }
+        }
  
      self.getValue = function(key) {
          if(key in privateHash) {return privateHash[key];
          } else {
              return undefined;
          }
-     }    
+        }    
  
      self.deleteValue = function(key) {
          if(!(key in privateHash)) {delete privateHash[key];
@@ -24,33 +24,17 @@
          } else { 
          return false;
          }
-     }    
+        }    
  
      self.getKeys() = function() {
          return Object.keys(privateHash);
-     }
+        }
 
       
     
 
-}
+    }
 
 let drinkStorage = new HashStorageFunc();
-     
 
-function drinkInputInfo() {
-
-    let alcohol;
-
-    let drinkName = prompt("Какое название напитка?");
-
-    let drinkAlcohol = confirm(drinkName + " - алкогольный напиток или нет?\nok - алкогольный\nотмена - без алкогольный");
-
-    let drinkRecipe = prompt("Какой рецепт напитка?");
-
-    drinkStorage(drinkName, drinkAlcohol);
-
-}
-
-// })();
- 
+})();
