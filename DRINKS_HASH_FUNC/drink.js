92a89725a0de1fac2bf094d698742e9b994328a6
ivandmitrienko <oneblackcrystal@gmail.com>
2022-01-21 22:10:12 +0300

    initial commit

--- DRINKS_HASH_FUNC/drink.js
+++ DRINKS_HASH_FUNC/drink.js
@@ -2,17 +2,16 @@
 
     "use strict";  
 
-    function drinkInputInfo() {
+    let drinkInputInfo = document.getElementById('add-coctail');
+   
+    drinkInputInfo.onclick = function() {
     
         var drinkName = prompt("Напишите название напитка");
-
 		var drinkDescribe = {};
-
-		drinkDescribe.alcohol = confirm(drinkName + " - 'это алкогольный напиток или нет?\nok - алкогольный\nотмена - без алкогольный");
-
+		drinkDescribe.alcohol = confirm(drinkName + " - 'Это алкогольный напиток или нет?\nok - алкогольный\nотмена - без алкогольный");
 		drinkDescribe.recipe = prompt("Напишите рецепт напитка - " + drinkName);
-
 	    drinkStorage.addValue(drinkName, drinkDescribe);
+	    console.log(drinkStorage.getValue(drinkName));
 	   
     
     }
