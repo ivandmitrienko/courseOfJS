f1c470558305b32261a907c563e993070c72e6d4
ivandmitrienko <oneblackcrystal@gmail.com>
2022-02-06 01:20:35 +0300

    initial commit

--- VALID_FORM/VALID_FORM.js
+++ VALID_FORM/VALID_FORM.js
@@ -2,13 +2,25 @@
 
 let form = document.querySelector('.form1');
 
-let fields = 
+let fields = form.querySelectorAll('.field')
 
 form.addEventListener('submit',validateInfoForm,false);
 
 function validateInfoForm(EO) {
     EO=EO||window.event;
     EO.preventDefault();
+    for (let i = 0; i < fields.length; i++) {
+        if (!fields[i].value) {
+            console.log(fields[i]);
+        //  let sp = document.createElement('span');
+        //  sp.innerHTML = 'ddd';
+        //  sp.style.color = 'red'
+        //  form.appendChild(sp);
+        }
+        if (!fields[i].value == '') {
+            console.log(fields[i]);
+    }   }  
+
 
 
 }
