6f3683a10f6c59900ea4948ccfa59fb95900045c
ivandmitrienko <oneblackcrystal@gmail.com>
2022-02-06 00:07:59 +0300

    initial commit

--- VALID_FORM/VALID_FORM.js
+++ VALID_FORM/VALID_FORM.js
@@ -1,22 +1,14 @@
 'use strict'
 
-form.addEventListener('submit', function (event) {
-    event.preventDefault()
-  
-    var errors = form.querySelectorAll('.fields')
-  
-    for (var i = 0; i < errors.length; i++) {
-      errors[i].remove()
-    }
-  
-    for (var i = 0; i < fields.length; i++) {
-      if (!fields[i].value) {
-        console.log('field is blank', fields[i])
-        var error = document.createElement('div')
-        error.className = 'error'
-        error.style.color = 'red'
-        error.innerHTML = 'Cannot be blank'
-        form[i].parentElement.insertBefore(error, fields[i])
-      }
-    }
-  })
+let form = document.querySelector('.form1');
+
+let fields = 
+
+form.addEventListener('submit',validateInfoForm,false);
+
+function validateInfoForm(EO) {
+    EO=EO||window.event;
+    EO.preventDefault();
+
+
+}
