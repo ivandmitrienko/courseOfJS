5d443a6185432a6522d6dca7951d3b658def21df
ivandmitrienko <oneblackcrystal@gmail.com>
2022-02-06 20:18:00 +0300

    initial commit

--- VALID_FORM/VALID_FORM.js
+++ VALID_FORM/VALID_FORM.js
@@ -7,25 +7,35 @@ let fields = form.querySelectorAll('.field');
 let comment = form.elements.comments;
 let commentValue = comment.checked;
 
+let deployment = form.elements.deployment;
+let deploymentValue = deployment.value; 
+
 form.addEventListener('submit',validateInfoForm,false);
 
 function validateInfoForm(EO) {
     EO=EO||window.event;
-    EO.preventDefault();
     let errors = document.getElementsByTagName('span');
     for (let j = 0; j < errors.length; j++) {
         errors[j].innerHTML = '';
     }
+    
     for (let i = 0; i < fields.length; i++) {
-        if (!(fields[i].value) ||  !(commentValue)){
-            console.log(fields[i]);
-            let error = document.createElement('span');
-            error.innerHTML = 'неверно указаны данные!';
-            error.style.color = 'red'
-            fields[i].after(error);    
-        } else {
-            // form.removeEventListener('submit',validateInfoForm,false);
-        }
+        if (!fields[i].value){
+             let error = document.createElement('span');
+             error.innerHTML = 'неверно указаны данные!';
+             error.style.color = 'red'
+             fields[i].after(error);
+             EO.preventDefault();
+
+            } else {
+            continue;
+        } 
+
+
+        // } else if (!(commentValue)) || !(deploymentValue){
+        //     comment.after(error);
+
+        // }
         
 
     }      
