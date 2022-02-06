94d14d4c229540ef244c77fbb460654ad896ade5
ivandmitrienko <oneblackcrystal@gmail.com>
2022-02-07 00:00:42 +0300

    initial commit

--- VALID_FORM/VALID_FORM.js
+++ VALID_FORM/VALID_FORM.js
@@ -1,29 +1,48 @@
 'use strict'
 
 let form = document.querySelector('.form1');
-
-let fields = form.querySelectorAll('.field');
-
-let comment = form.elements.comments;
-let commentValue = comment.checked;
-
-let deployment = form.elements.deployment;
-let deploymentValue = deployment.value; 
-
 form.addEventListener('submit',validateInfoForm,false);
 
 function validateInfoForm(EO) {
+
+    let fields = form.querySelectorAll('.field');
+    
+    let comment = form.elements.comments;
+    let commentValue = comment.checked;
+
+    let deployments = form.elements.deployment;
+    let deploymentsValue = deployments.value;
+
     EO=EO||window.event;
     let errors = document.getElementsByTagName('span');
     for (let j = 0; j < errors.length; j++) {
         errors[j].innerHTML = '';
     }
 
+    if(deploymentsValue == '') {
+        let divSp = form.querySelector(".errorDiv");
+        let error = document.createElement('span');
+        error.innerHTML = 'неверно указаны данные!';
+        error.style.color = 'red';
+        divSp.appendChild(error)
+        EO.preventDefault();
+           
+    }
+
+    if(!(commentValue)) {
+        let error = document.createElement('span');
+        error.innerHTML = 'неверно указаны данные!';
+        error.style.color = 'red';
+        comment.after(error);
+        EO.preventDefault();
+           
+    }
+
     for (let i = 0; i < fields.length; i++) {
         if (!fields[i].value){
              let error = document.createElement('span');
              error.innerHTML = 'неверно указаны данные!';
-             error.style.color = 'red'
+             error.style.color = 'red';
              fields[i].after(error);
              EO.preventDefault();
             } else {
@@ -32,10 +51,7 @@ function validateInfoForm(EO) {
          
 
 
-        // } else if (!(commentValue)) || !(deploymentValue){
-        //     comment.after(error);
-
-        // }
+            // || deploymentsValue == '' || !(commentValue)
         
 
     }      
