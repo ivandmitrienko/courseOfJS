56aae829513797cc522d23962e2016cf807dc752
ivandmitrienko <oneblackcrystal@gmail.com>
2022-02-07 22:22:30 +0300

    initial commit

--- VALID_FORM/VALID_FORM.js
+++ VALID_FORM/VALID_FORM.js
@@ -4,9 +4,9 @@ let form = document.querySelector('.form1');
 form.addEventListener('submit',validateInfoForm,false);
 let fields = form.querySelectorAll('.field');
 
-
-
 let authors = form.elements.author;
+let authorsValue = authors.value;
+
 let titles = form.elements.title;
 let urls = form.elements.url;
 let dates = form.elements.date;
@@ -14,7 +14,7 @@ let numbers = form.elements.number;
 let mails = form.elements.mail;
 let rubrics = form.elements.rubric;
 let comment = form.elements.comments;
-let deployments = form.elements.deployment; 
+let deployments = form.elements.deployment;
 
 function validateInfoForm(EO) {
 
@@ -33,7 +33,6 @@ function validateInfoForm(EO) {
     for (let i = 0; i < fields.length; i++) {
         if (!fields[i].value){
             arrEmptyField.push(fields[i]);
-            // arrEmptyField[0].focus();
             let error = document.createElement('span');
             error.innerHTML = 'empty field!';
             error.style.color = 'red';
@@ -65,58 +64,51 @@ function validateInfoForm(EO) {
            
     }
 
-
-    if(arrEmptyField.length) {
+    if (arrEmptyField.length !== 0) {
+        console.log(arrEmptyField[0]);
         arrEmptyField[0].focus();
-        EO.preventDefault();
-    } 
-
-}
-
-function blurInfo() {
-
-    for (let k = 0; k < fields.length; k++) {
-        fields[k].onblur = function(EO) {
-
-            EO=EO||window.event;
-
-            let errors = document.getElementsByTagName('span');
-            for (let j = 0; j < errors.length; j++) {
-                errors[j].innerHTML = '';
-            }
-            let authorsValue = authors.value;
-            if(authorsValue.length > 15) { 
-             arrEmptyField.push(fields[k]);  
-             let error = document.createElement('span');
-             error.innerHTML = 'Just 15 letters!';
-             error.style.color = 'red';
-             authors.after(error);
-             EO.preventDefault();
-            } 
-            let titlesValue = titles.value;
-            if(titlesValue.length > 10) {
-             arrEmptyField.push(fields[k]);
-             let error = document.createElement('span');
-             error.innerHTML = 'Just 10 letters!';
-             error.style.color = 'red';
-             titles.after(error);
-             EO.preventDefault();
-            } 
-     
-        }
     }
 
-    // if(arrEmptyField.length) {
-    //     arrEmptyField[0].focus();
-    //     EO.preventDefault();
-    // } 
-
-
 }
 
-blurInfo(fields);
+// function blurInfo() {
+
+//     for (let k = 0; k < fields.length; k++) {
+//         fields[k].onblur = function(EO) {
+
+//             EO=EO||window.event;
+
+//             let errors = document.getElementsByTagName('span');
+//             for (let j = 0; j < errors.length; j++) {
+//                 errors[j].innerHTML = '';
+//             }
+//             let authorsValue = authors.value;
+//             if(authorsValue.length > 15) { 
+//              arrMistakeField.push(fields[0]);
+//              let error = document.createElement('span');
+//              error.innerHTML = 'Just 15 letters!';
+//              error.style.color = 'red';
+//              authors.after(error);
+//              EO.preventDefault();
+//             } 
+//             let titlesValue = titles.value;
+//             if(titlesValue.length > 10) {
+//              arrMistakeField.push(fields[1]);
+//              console.log(arrMistakeField);
+//              let error = document.createElement('span');
+//              error.innerHTML = 'Just 10 letters!';
+//              error.style.color = 'red';
+//              titles.after(error);
+//              EO.preventDefault();
+//             } 
+     
+//         }
+//     }
 
 
+// }
+
+// blurInfo(fields);
 
 // fields[0].onblur = function(EO) {
 //     EO=EO||window.event;
@@ -136,7 +128,7 @@ blurInfo(fields);
 //     EO=EO||window.event;
     
 
-//     let authorsValue = authors.value;
+//     
 //     // let titlesValue = titles.value;
 //     // let urlsValue = urls.value;
 //     // let datesValue = dates.value;
