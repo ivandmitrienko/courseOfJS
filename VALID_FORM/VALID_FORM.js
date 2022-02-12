07b9bcb37b0f3365c37259429eabc9c8536dade6
ivandmitrienko <oneblackcrystal@gmail.com>
2022-02-12 23:44:33 +0300

    initial commit

--- VALID_FORM/VALID_FORM.js
+++ VALID_FORM/VALID_FORM.js
@@ -20,26 +20,47 @@ let arrEmptyField = []; // для поиска первого пустого п�
 
 function validateInfoForm(EO) {
 
+    
+    
+
     arrEmptyField.splice(0, arrEmptyField.length);
+    // console.log(arrEmptyField.length);
     
     let commentValue = comment.checked;
     let deploymentsValue = deployments.value;
 
     EO=EO||window.event;
 
-    let errors = document.getElementsByTagName('span');
+    debugger
 
-    for (var k = 0; k < errors.length; k++) {
-       if(errors[k].innerHTML === 'empty field!') {
-         errors[k].remove();
-        }
-      }
-    
+    // let errors = document.getElementsByTagName('span');
+
+    let errors = document.querySelectorAll('span');
     console.log(errors);
-    // debugger
+
+
+
+
+    
+    
+
+    for(let k = 0; k < errors.length; k++) {
+        if(errors[k].innerHTML === 'empty field!') {
+            console.log(errors[k]);
+
+            // fields[k].nextSibling.remove();
+           
+        }
+    }
+
+
+    
+    
+    // console.log(errors.length);
+    debugger
 
     for (let j = 0; j < errors.length; j++) {
-
+       
         if(errors[j].innerHTML === 'Just 30 letters!' || errors[j].innerHTML === 'Just 15 letters!'
          || errors[j].innerHTML === 'Please, add ".com" at the end of url!' || errors[j].innerHTML === 'Just after "May"!'
          || errors[j].innerHTML === "More than 100 visitors !"
@@ -51,7 +72,7 @@ function validateInfoForm(EO) {
         } 
     }
 
-    // debugger
+    debugger
    
     for (let i = 0; i < fields.length; i++) {
         
@@ -59,6 +80,7 @@ function validateInfoForm(EO) {
             arrEmptyField.push(fields[i]);
             let error = document.createElement('span');
             error.innerHTML = 'empty field!';
+
             error.style.color = 'red';
             fields[i].after(error);
             EO.preventDefault();
@@ -86,7 +108,10 @@ function validateInfoForm(EO) {
            
     }
 
-    console.log(arrEmptyField);
+    
+
+
+    // console.log(arrEmptyField);
 
 
     
@@ -96,155 +121,155 @@ function validateInfoForm(EO) {
 
 }
 
-function fieldsOnblurForm(fieldsdBlur) {
+// function fieldsOnblurForm(fieldsdBlur) {
 
-fieldsdBlur.forEach((v) => {(v).onblur = function(EO){
+// fieldsdBlur.forEach((v) => {(v).onblur = function(EO){
 
-        EO=EO||window.event;
+//         EO=EO||window.event;
 
-        if(v.nextSibling) {
-            v.nextSibling.innerHTML = '';
-        }  
+//         if(v.nextSibling) {
+//             v.nextSibling.innerHTML = '';
+//         }  
         
-        if(v.name == authors.name) {   
-            let authorsValue = authors.value;
-            if(authorsValue) {
-                if(authorsValue.length > 30){
-                    let error = document.createElement('span');
-                    error.innerHTML = 'Just 30 letters!';
-                    error.style.color = 'red';
-                    v.after(error);
-                }    
-            }              
-        }
-        if(v.name == titles.name) {
-        let titlesValue = titles.value;
-            if(titlesValue){
-                if(titlesValue.length > 15){
-                    let error = document.createElement('span');
-                    error.innerHTML = 'Just 15 letters!';
-                    error.style.color = 'red';
-                    v.after(error);
-                }
-            }   
-        }
-        if(v.name == urls.name) {
-            let urlsValue = urls.value;
-            if(urlsValue){
-                urlsValue = urlsValue.toLowerCase();
-                if(!(urlsValue.endsWith(".com"))){
-                    let error = document.createElement('span');
-                    error.innerHTML = 'Please, add ".com" at the end of url!';
-                    error.style.color = 'red';
-                    v.after(error);
-                }
+//         if(v.name == authors.name) {   
+//             let authorsValue = authors.value;
+//             if(authorsValue) {
+//                 if(authorsValue.length > 30){
+//                     let error = document.createElement('span');
+//                     error.innerHTML = 'Just 30 letters!';
+//                     error.style.color = 'red';
+//                     v.after(error);
+//                 }    
+//             }              
+//         }
+//         if(v.name == titles.name) {
+//         let titlesValue = titles.value;
+//             if(titlesValue){
+//                 if(titlesValue.length > 15){
+//                     let error = document.createElement('span');
+//                     error.innerHTML = 'Just 15 letters!';
+//                     error.style.color = 'red';
+//                     v.after(error);
+//                 }
+//             }   
+//         }
+//         if(v.name == urls.name) {
+//             let urlsValue = urls.value;
+//             if(urlsValue){
+//                 urlsValue = urlsValue.toLowerCase();
+//                 if(!(urlsValue.endsWith(".com"))){
+//                     let error = document.createElement('span');
+//                     error.innerHTML = 'Please, add ".com" at the end of url!';
+//                     error.style.color = 'red';
+//                     v.after(error);
+//                 }
 
-            }
+//             }
             
-        }
-        if(v.name == dates.name) {
-            if(v.value){
-                let arrDates =v.value.split('-');
-                if(arrDates[1] < 5){
-                    let error = document.createElement('span');
-                    error.innerHTML = 'Just after "May"!';
-                    error.style.color = 'red';
-                    v.after(error);
-                }
-            }
+//         }
+//         if(v.name == dates.name) {
+//             if(v.value){
+//                 let arrDates =v.value.split('-');
+//                 if(arrDates[1] < 5){
+//                     let error = document.createElement('span');
+//                     error.innerHTML = 'Just after "May"!';
+//                     error.style.color = 'red';
+//                     v.after(error);
+//                 }
+//             }
             
-        }    
-        if(v.name == numbers.name) {
-            if(v.value){
-                if(v.value < 100){
-                    let error = document.createElement('span');
-                    error.innerHTML = 'More than 100 visitors !';
-                    error.style.color = 'red';
-                    v.after(error);
-                }
+//         }    
+//         if(v.name == numbers.name) {
+//             if(v.value){
+//                 if(v.value < 100){
+//                     let error = document.createElement('span');
+//                     error.innerHTML = 'More than 100 visitors !';
+//                     error.style.color = 'red';
+//                     v.after(error);
+//                 }
 
-            }
+//             }
             
-        }
-        if(v.name == mails.name) {
-            if(v.value){
-                let mailsValue =  mails.value;
-                if(!(mailsValue.endsWith('@gmail.com'))){
-                    let error = document.createElement('span');
-                    error.innerHTML = 'We use just gmail.com! Please, add: @gmail.com at the end of email !';
-                    error.style.color = 'red';
-                    v.after(error);
-                }
+//         }
+//         if(v.name == mails.name) {
+//             if(v.value){
+//                 let mailsValue =  mails.value;
+//                 if(!(mailsValue.endsWith('@gmail.com'))){
+//                     let error = document.createElement('span');
+//                     error.innerHTML = 'We use just gmail.com! Please, add: @gmail.com at the end of email !';
+//                     error.style.color = 'red';
+//                     v.after(error);
+//                 }
 
-            }
+//             }
             
-        }
-        if(v.name == rubrics.name) {
-            if(v.value){
-                if(v.value == 3){
-                    let error = document.createElement('span');
-                    error.innerHTML = "Sorry) At the moment the section: бытовая техника is not available !";
-                    error.style.color = 'red';
-                    v.after(error);
-                }
+//         }
+//         if(v.name == rubrics.name) {
+//             if(v.value){
+//                 if(v.value == 3){
+//                     let error = document.createElement('span');
+//                     error.innerHTML = "Sorry) At the moment the section: бытовая техника is not available !";
+//                     error.style.color = 'red';
+//                     v.after(error);
+//                 }
 
-            }
+//             }
             
-        }
-        if(v.name == articles.name) {
-            if(v.value){
-                let articlesValue = v.value;
-                if(articlesValue.length < 30){
-                    let error = document.createElement('span');
-                    error.innerHTML = "Please, enter a more complete description of the site!";
-                    error.style.color = 'red';
-                    v.after(error);
-                }
+//         }
+//         if(v.name == articles.name) {
+//             if(v.value){
+//                 let articlesValue = v.value;
+//                 if(articlesValue.length < 30){
+//                     let error = document.createElement('span');
+//                     error.innerHTML = "Please, enter a more complete description of the site!";
+//                     error.style.color = 'red';
+//                     v.after(error);
+//                 }
 
-            }
+//             }
             
-        }                         
+//         }                         
 
-    }})
+//     }})
 
-    comment.onchange = function () {
-        if (comment.checked) {
-            let agree = document.createElement('span');
-            agree.innerHTML = "We will always check reviews and sort :)";
-            agree.style.color = '#32CD32';
-            agree.id = "2sec";
-            agree.style.fontSize = "20px"
-            comment.after(agree);
-            setTimeout(function(){
-                document.getElementById('2sec').remove(); //style.display = 'none'
-            }, 2500);
-        }    
+//     comment.onchange = function () {
+//         if (comment.checked) {
+//             let agree = document.createElement('span');
+//             agree.innerHTML = "We will always check reviews and sort :)";
+//             agree.style.color = '#32CD32';
+//             agree.id = "2sec";
+//             agree.style.fontSize = "20px"
+//             comment.after(agree);
+//             setTimeout(function(){
+//                 document.getElementById('2sec').remove(); //style.display = 'none'
+//             }, 2500);
+//         }    
              
-    }
+//     }
 
-    deployments.forEach(function (val, ind, arr) {
-        arr[ind].onchange = function (EO) {
-            EO=EO||window.event;
-            let divSp = form.querySelector(".errorDiv");
-            if (document.getElementById("only")) {
-                let deploymentsSpan = document.getElementById('only');
-                deploymentsSpan.remove();
-            } 
-            let error = document.createElement('span');
-            error.style.color = 'red';
-            error.id = "only";
-            divSp.appendChild(error);
-            if (arr[ind].value == 11) {
-             error.innerHTML = 'Only for a fee or VIP!';
-            }
+//     deployments.forEach(function (val, ind, arr) {
+//         arr[ind].onchange = function (EO) {
+//             EO=EO||window.event;
+//             let divSp = form.querySelector(".errorDiv");
+//             if (document.getElementById("only")) {
+//                 let deploymentsSpan = document.getElementById('only');
+//                 deploymentsSpan.remove();
+//             } 
+//             let error = document.createElement('span');
+//             error.style.color = 'red';
+//             error.id = "only";
+//             divSp.appendChild(error);
+//             if (arr[ind].value == 11) {
+//              error.innerHTML = 'Only for a fee or VIP!';
+//             }
             
 
     
-        }
-    });
+//         }
+//     });
  
              
-}
+// }
 
-fieldsOnblurForm(fields);
+// fieldsOnblurForm(fields);
 
