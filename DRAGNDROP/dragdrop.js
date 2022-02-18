5344abd7e03a63cc1685e02ec9be43611d610269
ivandmitrienko <oneblackcrystal@gmail.com>
2022-02-18 17:05:46 +0300

    initial commit

--- DRAGNDROP/dragdrop.js
+++ DRAGNDROP/dragdrop.js
@@ -9,17 +9,81 @@ function posElem(EO) {
     let imgs = document.getElementsByTagName('img');
 
     for(let i = 0; i < imgs.length; i++) {
-
+   
         let elem = imgs[i];
 
         elem.style.left = elem.offsetLeft + 'px';
 
         elem.style.top = elem.offsetTop + 'px';
 
-        elem.style.position = 'absolute';
+        elem.onmousedown = imgDown;
 
+        elem.onmouseup = imgUp;
     }
-   
+
+    for(let k = 0; k < imgs.length; k++) {
+
+        let elem = imgs[k];
+
+        elem.style.position = 'absolute';
+    }
+
+}  
+
+// debugger 
+
+function imgDown(EO){
+
+    EO=EO||window.event;
+
+    EO.preventDefault();
+
+    let currentImgDown = EO.target; 
+
+    // запоминаем разницу между положением объекта и нажатием мыши
+
+    let currentImgDownX = EO.pageX - currentImgDown.offsetLeft;
+
+    let currentImgDownY = EO.pageY - currentImgDown.offsetTop;
+
+    document.body.appendChild(currentImgDown);
+
+    window.onmousemove = imgMove;
+
+    function imgMove(EO) {
+    
+        EO=EO||window.event;
+    
+        EO.preventDefault();
+    
+        let currentImgMove = EO.target; 
+    
+        currentImgMove.style.left = (EO.pageX - currentImgDownX) + 'px';
+    
+        currentImgMove.style.top = (EO.pageY - currentImgDownY) + 'px';
+    
+    
+    }
+
+}
+
+function imgUp(EO){
+
+    EO=EO||window.event;
+
+    EO.preventDefault();
+
+    window.onmousedown = null;
+
+    window.onmousemove = null;
 
 
 }
+
+
+
+
+
+
+
+
