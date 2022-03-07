c17fd5e2102e51ef816fdb5358eeac2edf2be647
ivandmitrienko <oneblackcrystal@gmail.com>
2022-03-08 01:55:16 +0300

    initial commit

--- CLOCK_CANVAS/CLOCK_CANVAS.js
+++ CLOCK_CANVAS/CLOCK_CANVAS.js
@@ -24,27 +24,25 @@ document.getElementById('start').onclick = function () {
 	angleValue = 0, // угол
 	distanceOfDigits = 30, // расстояние(в градусах) между цифрами на часах
 	// для электронных часов
-	digitalWatch,
 	digitalWatchText,
 	digitalWatchWidth = diameter/3,
 	digitalWatchHeight = diameter/12,
 	radiusForDigitalWatch = diameter/4, // радиус (растояние) для электронных часов;
 	// (для стрелки часов)
-	elemForArrowHours,
 	elemForArrowHoursHeight = diameter/6,
 	elemForArrowHoursWidth = diameter/30,
 	// для стрелки минут
-	elemForArrowMinutes,
 	elemForArrowMinutesHeight = diameter/4,
 	elemForArrowMinutesWidth = diameter/35,
 	// для стрелки секунд
-	elemForArrowSeconds,
 	elemForArrowSecondsHeight = diameter/3,
 	elemForArrowSecondsWidth = diameter/60,
 	hoursDeg, //переменная для дальнейшего определение по времени где должна быть стрелка часов
 	minutesDeg, //переменная для дальнейшего определение по времени где должна быть стрелка минут
 	secondsDeg, //переменная для дальнейшего определение по времени где должна быть стрелка секунд
 	hourDigits = 12; //цифры часов (например 1,2,3,4,5,6,7,8,9,10,11,12)
+
+	// for big clock
 
     let context = canvas.getContext('2d');
     context.fillStyle='#FCCA66';
@@ -71,11 +69,68 @@ document.getElementById('start').onclick = function () {
 		context.fill();
 
 		context.fillStyle ='black';
-		context.font ="normal normal 20px 'Times New Roman'";
+		context.font ="normal bold 20px 'Times New Roman'";
 		context.textAlign='center';
 		context.textBaseline='middle';
 		context.fillText(i,smallCircleCX, smallCircleCY);
 	}
 
+	
+	function arrows() {
+
+		let time = new Date();
+		console.log(time.toLocaleTimeString());
+
+		// for digitalclock
+
+	    context.globalCompositeOperation = "source-over";
+	    context.fillStyle = "#FCCA66";
+	    context.beginPath();
+	    context.fillRect(canvasX - digitalWatchWidth/2, canvasY - radiusForDigitalWatch - digitalWatchHeight/2, diameter/3, diameter/12);
+	    context.fill();
+    
+	    context.fillStyle = "black";
+	    digitalWatchText = time.toLocaleTimeString();
+	    context.font ="normal bold 20px 'Times New Roman'";
+	    context.textAlign='center';
+	    context.textBaseline='middle';
+	    context.fillText(digitalWatchText, canvasX, canvasY - radiusForDigitalWatch);	
+	    context.fill();
+
+		//for hours 
+
+		hoursDeg = 30 * (time.getHours() + (1 / 60) * time.getMinutes());
+	    context.strokeStyle = "black";
+	    context.lineWidth = elemForArrowHoursWidth;
+	    context.lineCap = "round";
+	    context.beginPath();
+	    context.moveTo(canvasX, canvasY);
+	    context.lineTo(canvasX + elemForArrowHoursHeight * Math.sin(hoursDeg/180*Math.PI), canvasY - elemForArrowHoursHeight * Math.cos(hoursDeg/180*Math.PI));
+	    context.stroke();
+		//for minutes
+		minutesDeg = 6 * (time.getMinutes() + (1 / 60) * time.getSeconds());
+	    context.strokeStyle = "black";
+	    context.lineWidth = elemForArrowMinutesWidth;
+	    context.lineCap = "round";
+	    context.beginPath();
+	    context.moveTo(canvasX, canvasY);
+	    context.lineTo(canvasX + elemForArrowMinutesHeight * Math.sin(minutesDeg/180*Math.PI), canvasY - elemForArrowMinutesHeight * Math.cos(minutesDeg/180*Math.PI));
+	    context.stroke();
+		//for seconds
+		secondsDeg = 6 * time.getSeconds();
+	    context.strokeStyle = "red";
+	    context.lineWidth = elemForArrowSecondsWidth;
+	    context.lineCap = "round";
+	    context.beginPath();
+	    context.moveTo(canvasX, canvasY);
+	    context.lineTo(canvasX + elemForArrowSecondsHeight * Math.sin(secondsDeg/180*Math.PI), canvasY - elemForArrowSecondsHeight * Math.cos(secondsDeg/180*Math.PI));
+	    context.stroke();
+		
+		setTimeout(arrows,1020-time.getMilliseconds());
+
+	}
+
+	
+    arrows();
 
 }
