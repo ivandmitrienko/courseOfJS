540c22767978cf4728cd4708483a946eecb8688a
ivandmitrienko <oneblackcrystal@gmail.com>
2022-03-12 22:11:42 +0300

    initial commit

--- DRINKS_LOC_STORAGE/drinks.js
+++ DRINKS_LOC_STORAGE/drinks.js
@@ -56,6 +56,63 @@
         drinkText.innerHTML = drinkStorage.getKeys().join(", ");
     }
 
+    // for dish
+
+
+    let dishInputInfo = document.getElementById('add-dish');
+   
+    dishInputInfo.onclick = function() {
+    
+        let dishNameAdd = prompt("Напишите название блюда");
+
+		let dishDescribe = {};
+
+		dishDescribe.vegetarian = confirm(dishNameAdd + " - 'Это вегетарианское блюдо или нет?\nok - вегетарианское\nотмена - мясное");
+
+		dishDescribe.recipe = prompt("Напишите рецепт блюда - " + dishNameAdd);
+
+	    dishStorage.addValue(dishNameAdd, dishDescribe);  
+    }
+
+    let dishSGetInfo = document.getElementById("get-dish");
+
+    drinkGetInfo.onclick = function(){
+    	
+        let dishNameGet = prompt("Напишите название блюда");
+
+        let dishText = document.getElementById("dishText");
+
+        let dishAnswer = dishStorage.getValue(dishNameGet);
+
+        if (dishAnswer) {
+			dishText.innerHTML = "блюдо: " + dishNameGet + 
+								   "<br>" + "вегетарианское: " + (dishAnswer.vegetarian ? "да" : "нет") + 
+								   "<br>" + "Рецепт: " + (dishAnswer.recipe ? dishAnswer.recipe : "к сожалению РЕЦЕПТА НЕТ");
+		} else {
+			dishText.innerHTML = "Нет такого блюда(";
+		}
+    }
+
+    let dishDeleteInfo = document.getElementById("delete-dish");
+
+    dishDeleteInfo.onclick = function(){
+
+        let dishNameDelete = prompt("Напишите название блюда");
+
+        if((dishStorage.deleteValue(dishNameDelete)) === true) {
+            dishText.innerHTML = "блюдо " + dishNameDelete + " удалено )";
+        } else {
+            dishText.innerHTML = "Такое блюдо не найдено (";
+        }
+
+    }
+
+    let dishNamesCoctails = document.getElementById("names-dishes");
+
+    ddishNamesCoctails.onclick = function(){
+        dishText.innerHTML = dishStorage.getKeys().join(", ");
+    }
+
 
 
 
